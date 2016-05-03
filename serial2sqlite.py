df8bbe422453d9c84d9f8cde1c424baa53d778c7
Joonas Jauhainen <joonas.jauhiainen@gmail.com>
2016-05-03 23:02:27 +0300

    Updated to use more arguments.

--- serial2sqlite.py
+++ serial2sqlite.py
@@ -1,4 +1,4 @@
-from utils.dummy_adapter import DummyAdapter
+from utils.serial_reader import SerialDataAdapter
 from utils import db
 from models import SerialData
 import time
@@ -8,8 +8,9 @@ from plotly.graph_objs import Scatter, Layout
 
 class Serial2Sqlite(object):
 
-    def __init__(self):
-        self.adapter = DummyAdapter()
+    def __init__(self, sample_rate, **kwargs):
+        self.sample_rate = sample_rate
+        self.adapter = SerialDataAdapter(port=kwargs['port'], baudrate=kwargs['baudrate'])
 
 
     def run(self):
@@ -18,7 +19,7 @@ class Serial2Sqlite(object):
             db.session.add(data)
             db.session.commit()
             print("Value added")
-            time.sleep(5)
+            time.sleep(sample_rate)
 
     def generate_plot(self):
         values = []
@@ -36,8 +37,14 @@ class Serial2Sqlite(object):
 
 
 if __name__ == "__main__":
-    mode = sys.argv[1]
-    s2s = Serial2Sqlite()
+    if len(sys.argv) != 4:
+        print("Usage: {} port baudrate sample_rate mode, mode is run or plot".format(sys.argv[0]))
+        sys.exit(1)
+    port = sys.argv[1]
+    baudrate = sys.argv[2]
+    sample_rate = sys.argv[3]
+    mode = sys.argv[4]
+    s2s = Serial2Sqlite(sample_rate, port=port, baudrate=baudrate)
     if mode == "run":
         s2s.run()
     else:
